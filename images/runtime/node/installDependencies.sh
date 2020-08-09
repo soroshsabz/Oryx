fd5fe9bf4aca063b62a7ca137aed87c1ee5b1c6e
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2020-08-09 11:50:29 -0700

    Upgrade packages whenever runtime images are built and upgrade npm cli

--- images/runtime/node/installDependencies.sh
+++ images/runtime/node/installDependencies.sh
@@ -11,27 +11,32 @@ set -ex
 mkdir -p /node_modules
 chmod 777 /node_modules
 
-# Npm version 6.4 has issues installing native modules like grpc,
-# so upgrading it to a version which we know works fine.
-npm_ver=$(npm --version)
-IFS='.' read -ra SPLIT_VERSION <<< "$npm_ver"
-major="${SPLIT_VERSION[0]}"
-minor="${SPLIT_VERSION[1]}"
-if [ "$major" == "6" ] && [ "$minor" == "4" ]; then
-    echo
-    echo "Upgrading npm version from $npm_ver to 6.9.0"
-    npm install -g npm@6.9.0
+# Since older versions of npm cli have security vulnerabilities, we try upgrading it
+# to the latest available version. However latest versions of npm do not work with very
+# old versions of Node (for example: 4 or 6), so we special case here to limit the upgrade
+# to only versions 8 or above
+currentNodeVersion=$(node --version)
+currentNodeVersion=${currentNodeVersion#?}
+echo "Current Node version is $currentNodeVersion"
+IFS='.' read -ra NODE_VERSION_PARTS <<< "$currentNodeVersion"
+nodeVersionMajor="${NODE_VERSION_PARTS[0]}"
+# v10 => 10
+nodeVersionMajor="${nodeVersionMajor:1}"
+
+currentNpmVersion=$(npm --version)
+echo "Version of npm: $currentNpmVersion"
+
+# Upgrade npm to the latest available version
+if [[ $nodeVersionMajor -ge 8  ]]; then
+    echo "Upgrading npm..."
+    npm install npm -g
+    echo "Done upgrading npm."
+    currentNpmVersion=$(npm --version)
+    echo "Version of npm after upgrade: $currentNpmVersion"
 fi
 
 # Do NOT install PM2 from Node 14 onwards
-# v10.14.0
-currentNodeVersion=$(node --version)
-echo "Current Node version is $currentNodeVersion"
-currentNodeVersion=${currentNodeVersion#?}
-IFS='.' read -ra SPLIT_VERSION <<< "$currentNodeVersion"
-major="${SPLIT_VERSION[0]}"
-
-if [ "$major" -lt "14" ]; then
+if [ "$nodeVersionMajor" -lt "14" ]; then
     echo "Installing PM2..."
     # PM2 is supported as an option when running the app,
     # so we need to make sure it is available in our images.
