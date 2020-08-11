f01b0f5bc5bcd418d904b248e984dd3b16ea6c09
Kiran Challa <kichalla@microsoft.com>
2020-08-11 06:39:14 -0700

    updated

--- build/tools/AutoUpdater/GitHubRunnersCachedImagesCheckCommand.cs
+++ build/tools/AutoUpdater/GitHubRunnersCachedImagesCheckCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
 using System.Text;
 using McMaster.Extensions.CommandLineUtils;
 
@@ -9,7 +11,69 @@ namespace AutoUpdater
     {
         public int OnExecute(CommandLineApplication app, IConsole console)
         {
+            var gitHubRunnersReadMeDigest = GetDigestFromGitHubRunnersReadMe();
+            var oryxDockerfileDigest = GetDigestFromOryxGitHubRunnersDockerfile();
+            if (!string.Equals(gitHubRunnersReadMeDigest, oryxDockerfileDigest, StringComparison.OrdinalIgnoreCase))
+            {
+                if (!string.IsNullOrEmpty(gitHubRunnersReadMeDigest))
+                {
+                    var newDockerFileContent = $"FROM buildpack-deps:stretch@sha256:{gitHubRunnersReadMeDigest}";
+                    var dockerFileLocation = "images/build/Dockerfiles/gitHubRunners.BuildPackDepsStretch.Dockerfile";
+                    var newBranchName = "autoupdater/update.githubrunners.digest";
+                    var tempDir = Path.GetTempPath();
+                    var script = @$"
+cd {tempDir}
+git clone https://github.com/microsoft/oryx --depth 1
+git checkout -b {newBranchName}
+echo '{gitHubRunnersReadMeDigest}' > {dockerFileLocation}
+git add {dockerFileLocation}
+git commit -m 'Updated GitHub runners digest sha'
+git push -u origin {newBranchName}
+";
+                }
+            }
+
             return 0;
+        }
+
+        private string GetDigestFromGitHubRunnersReadMe()
+        {
+            var gitHubRunnersReadMeFileUrl = "https://raw.githubusercontent.com/actions/virtual-environments/main/images/linux/Ubuntu2004-README.md";
+            var httpClient = new HttpClient();
+            var content = httpClient.GetStringAsync(gitHubRunnersReadMeFileUrl).Result;
+            var lines = content.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                if (line.Contains("buildpack-deps:stretch", StringComparison.OrdinalIgnoreCase))
+                {
+                    var prefix = "sha256:";
+                    var startIndex = line.IndexOf(prefix, StringComparison.OrdinalIgnoreCase);
+                    // sha256 is 64 characters long
+                    return line.Substring(startIndex + prefix.Length, 64);
+                }
+            }
+
+            return null;
+        }
+
+        private string GetDigestFromOryxGitHubRunnersDockerfile()
+        {
+            var oryxGitHubRunnersDockerfileUrl = "https://raw.githubusercontent.com/microsoft/Oryx/master/images/build/Dockerfiles/gitHubRunners.BuildPackDepsStretch.Dockerfile";
+            var httpClient = new HttpClient();
+            var content = httpClient.GetStringAsync(oryxGitHubRunnersDockerfileUrl).Result;
+            var lines = content.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                if (line.Contains("buildpack-deps:stretch", StringComparison.OrdinalIgnoreCase))
+                {
+                    var prefix = "sha256:";
+                    var startIndex = line.IndexOf(prefix, StringComparison.OrdinalIgnoreCase);
+                    // sha256 is 64 characters long
+                    return line.Substring(startIndex + prefix.Length, 64);
+                }
+            }
+
+            return null;
         }
     }
 }
