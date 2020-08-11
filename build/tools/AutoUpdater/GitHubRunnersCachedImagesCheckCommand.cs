16c6da1edbcd3b855f7c9e222682d755fbfd8815
Kiran Challa <kichalla@microsoft.com>
2020-08-11 08:26:58 -0700

    updated

--- build/tools/AutoUpdater/GitHubRunnersCachedImagesCheckCommand.cs
+++ build/tools/AutoUpdater/GitHubRunnersCachedImagesCheckCommand.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
-using System.Text;
 using McMaster.Extensions.CommandLineUtils;
 
 namespace AutoUpdater
@@ -24,6 +22,8 @@ namespace AutoUpdater
                     var newBranchName = "autoupdater/update.githubrunners.digest";
                     var tempDir = Path.GetTempPath();
                     var script = @$"
+#!/bin/bash
+set -ex
 cd {tempDir}
 git clone https://github.com/microsoft/oryx --depth 1
 git checkout -b {newBranchName}
@@ -37,6 +37,38 @@ curl \
   https://api.github.com/repos/microsoft/oryx/pulls \
   -d '{{""title"":""Updated GitHub runners digest"",""head"":""{{{newBranchName}}}"",""base"":""master""}}'
 ";
+
+                    var scriptPath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid():N}.sh");
+                    Console.WriteLine($"Script generated at {scriptPath}");
+                    File.WriteAllText(scriptPath, script);
+
+                    Console.WriteLine("Setting executable permission...");
+                    var exitCode = ProcessHelper.TrySetExecutableMode(scriptPath);
+                    if (exitCode != 0)
+                    {
+                        Console.WriteLine("Error setting executable permission on the script");
+                        return exitCode;
+                    }
+
+                    Console.WriteLine("Running the script...");
+                    exitCode = ProcessHelper.RunProcess(
+                        scriptPath,
+                        arguments: null,
+                        workingDirectory: null,
+                        // Preserve the output structure and use AppendLine as these handlers
+                        // are called for each line that is written to the output.
+                        standardOutputHandler: (sender, args) =>
+                        {
+                            Console.WriteLine(args.Data);
+                        },
+                        standardErrorHandler: (sender, args) =>
+                        {
+                            Console.Error.WriteLine(args.Data);
+                        },
+                        waitTimeForExit: TimeSpan.FromMinutes(15));
+
+                    Console.WriteLine("Done.");
+                    return exitCode;
                 }
             }
 
