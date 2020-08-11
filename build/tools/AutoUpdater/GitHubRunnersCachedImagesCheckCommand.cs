d92b7cce2d35bd7d63639e0f19b057f27d291b49
Kiran Challa <kichalla@microsoft.com>
2020-08-11 06:11:50 -0700

    updated

---
+++ build/tools/AutoUpdater/GitHubRunnersCachedImagesCheckCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace AutoUpdater
+{
+    public class GitHubRunnersCachedImagesCheckCommand
+    {
+        public int OnExecute(CommandLineApplication app, IConsole console)
+        {
+            return 0;
+        }
+    }
+}
---
+++ build/tools/AutoUpdater/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace AutoUpdater
+{
+    [Command("autoupdater", Description = "Tool to run checks and send out PRs if necessary.")]
+    [Subcommand(typeof(GitHubRunnersCachedImagesCheckCommand))]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
